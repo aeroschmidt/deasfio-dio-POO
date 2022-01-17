518e74da7e668a698fa8499ab57cc52bd183a989
Sabrina <sabrinaschmidt335@gmail.com>
2022-01-17 16:22:14 -0300

    Final 1

--- src/br/com/dio/desafio/dominio/Bootcamp.java
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -14,50 +14,63 @@ import java.util.Set;
  *
  */
 public class Bootcamp {
-	
+
 	private String nome;
 	private String descricao;
 	private final LocalDate dataInicial = LocalDate.now();
 	private LocalDate dataFinal = dataInicial.plusDays(45);
 	private Set<Dev> devsInscritos = new HashSet<>();
-	private Set<Conteudo> conteudos =  new LinkedHashSet<>();
+	private Set<Conteudo> conteudos = new LinkedHashSet<>();
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getDescricao() {
 		return descricao;
 	}
+
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
+
 	public LocalDate getDataFinal() {
 		return dataFinal;
 	}
+
 	public void setDataFinal(LocalDate dataFinal) {
 		this.dataFinal = dataFinal;
 	}
+
 	public Set<Dev> getDevsInscritos() {
 		return devsInscritos;
 	}
+
 	public void setDevsInscritos(Set<Dev> devsInscritos) {
 		this.devsInscritos = devsInscritos;
 	}
+
 	public Set<Conteudo> getConteudos() {
 		return conteudos;
 	}
+
 	public void setConteudos(Set<Conteudo> conteudos) {
 		this.conteudos = conteudos;
 	}
+
 	public LocalDate getDataInicial() {
 		return dataInicial;
 	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(conteudos, dataFinal, dataInicial, descricao, devsInscritos, nome);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -71,8 +84,5 @@ public class Bootcamp {
 				&& Objects.equals(dataInicial, other.dataInicial) && Objects.equals(descricao, other.descricao)
 				&& Objects.equals(devsInscritos, other.devsInscritos) && Objects.equals(nome, other.nome);
 	}
-	
-	
-	
 
 }
--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -5,6 +5,7 @@ package br.com.dio.desafio.dominio;
 
 import java.util.LinkedHashSet;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 
 /**
@@ -18,15 +19,22 @@ public class Dev {
 	private Set<Conteudo> conteudoConcluido = new LinkedHashSet<>();
 
 	public void inscreverBootcamp(Bootcamp bootcamp) {
-
+		this.conteudoInscritos.addAll(bootcamp.getConteudos());
+		bootcamp.getDevsInscritos().add(this);
 	}
 
 	public void progredir() {
-
+		Optional<Conteudo> conteudo = this.conteudoInscritos.stream().findFirst();
+		if (conteudo.isPresent()) {
+			this.conteudoConcluido.add(conteudo.get());
+			this.conteudoInscritos.remove(conteudo.get());
+		} else {
+			System.err.println("Você não esta matriculado em nenhum conteudo.");
+		}
 	}
 
-	public void calcularTotalXP() {
-
+	public double calcularTotalXP() {
+		return this.conteudoConcluido.stream().mapToDouble(conteudo -> conteudo.calcularXp()).sum();
 	}
 
 	public String getNome() {
@@ -77,7 +85,5 @@ public class Dev {
 		return Objects.equals(conteudoConcluido, other.conteudoConcluido)
 				&& Objects.equals(conteudoInscritos, other.conteudoInscritos) && Objects.equals(nome, other.nome);
 	}
-	
-	
 
 }
